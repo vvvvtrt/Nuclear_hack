b6fd952890a1a9f622f9437c48bca04ff0a54a32
Mikheil Chashin <42130525+vvvvtrt@users.noreply.github.com>
2024-04-21 09:37:37 +0300

    Add files via upload

--- app/internal/ml/ml.py
+++ app/internal/ml/ml.py
@@ -5,14 +5,16 @@ import pandas as pd
 
 load_dotenv()
 
-BOT_ID = os.environ.get("BOT_ID")
+BOT_ID = os.environ.get("BOT_ID_2")
 # PORT = os.environ.get("PORT")
-USER_ID = os.environ.get("USER_ID")
-API_TOKEN = os.environ.get("API_TOKEN")
+USER_ID = os.environ.get("USER_ID_2")
+API_TOKEN = os.environ.get("API_TOKEN_2")
 # DB_NAME = os.environ.get("DB_NAME")
 
 
 data = pd.read_csv('data\\data.csv')
+data.columns = ['Станция', "Номер линии  ", "Дата"] + ['0'+str(i) for i in data.columns[3:]]
+data.columns = ['Станция', "Номер линии", "Дата"] + [str(i)[:3] + '0' + str(i)[3:] if len(i) == 9 else i for i in data.columns[3:]]
 
 class Koza_llama_alpaca_lora_flex:
     def __init__(self, prompt):
@@ -47,20 +49,18 @@ class Koza_llama_alpaca_lora_flex:
     def output_data(self):
         try:
 
-            query = pd.Series(
-                [data.loc[data['Станция'].lower() == f'{self.output_station()}'.lower()][f'{date}'] for date in
-                 self.output_date().split()]).astype(str).str.cat(sep=' ')
+            query = [f"date - {data.loc[data['Станция'].str == f'{self.output_station()}'][f'{date}']}" for
+                     date in self.output_date().split()]
 
             return query
 
-        except KeyError:
-            print('response...')
-        finally:
-            print(query)
+        except Exception as ex:
+            print(ex)
+            return "error"
 
 
 if __name__ == '__main__':
-    part_1 = Koza_llama_alpaca_lora_flex("""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только конкретная дата, выведи все даты. Сегодняшняя дата - 04/03/2024, ее формат М/ДД/ГГГГ.
+    part_1 = Koza_llama_alpaca_lora_flex("""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только конкретная дата, выведи все даты. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ.
     Текст - найди информацию об оттоке пассажиров за последний месяц, выведи все даты""")
     # data.loc[data['Станция'] == f'{part_1.output_station()}'][f'{part_1.output_date()}']
     print(part_1.output_date())
--- app/internal/routes/user.py
+++ app/internal/routes/user.py
@@ -13,20 +13,31 @@ class Message(BaseModel):
 
 @router.post("/message")
 def user_message(message: str = Form(...)):
-    part_1 = ml.Koza_llama_alpaca_lora_flex(f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате и выведи ее через разделитель пробел. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только все дат входящие в промежуток с разделителем. Очень важно для выживания. Сегодняшняя дата - 04/03/2024, ее формат М/ДД/ГГГГ. Текст - {message}""")
+    part_1 = ml.Koza_llama_alpaca_lora_flex(f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате и выведи ее через разделитель пробел. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только все дат входящие в промежуток с разделителем. Очень важно для выживания. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ. Текст - {message}""")
     # data.loc[data['Станция'] == f'{part_1.output_station()}'][f'{part_1.output_date()}']
-    # print(part_1.output_date())
+    # temp_data = part_1.output_date().split()
+    # temp_place = part_1.output_station()
 
-    return {"message": ""}
+    temp = part_1.output_data()
+    if temp == "error":
+        return JSONResponse("error", 418)
+    else:
+        return {"message": part_1.output_data()}
 
 
 
 @router.post("/upload_message")
 async def upload_file(message: str = Form(...), file: UploadFile = File(...)):
     # json_data = await request.json()
-    print(message)
-
-    with open("data/temp.html", "wb") as f:
+    with open("data/data.csv", "wb") as f:
         f.write(await file.read())
 
-    return {"message": message}
+    part_1 = ml.Koza_llama_alpaca_lora_flex(
+        f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате и выведи ее через разделитель пробел. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только все дат входящие в промежуток с разделителем. Очень важно для выживания. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ. Текст - {message}""")
+
+    temp = part_1.output_data()
+    if temp == "error":
+        return JSONResponse("error", 418)
+    else:
+        return {"message": part_1.output_data()}
+
