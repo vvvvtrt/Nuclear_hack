c8c45118ab5300d62cfe11b43f1944c93e8a996d
Mikheil Chashin <42130525+vvvvtrt@users.noreply.github.com>
2024-04-21 13:32:58 +0300

    Add files via upload

--- app/internal/ml/ml.py
+++ app/internal/ml/ml.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 load_dotenv()
 
-BOT_ID = os.environ.get("BOT_ID_2")
+BOT_ID = os.environ.get("BOT_ID_4")
 # PORT = os.environ.get("PORT")
-USER_ID = os.environ.get("USER_ID_2")
-API_TOKEN = os.environ.get("API_TOKEN_2")
+USER_ID = os.environ.get("USER_ID_4")
+API_TOKEN = os.environ.get("API_TOKEN_4")
 # DB_NAME = os.environ.get("DB_NAME")
 
 
@@ -17,16 +17,30 @@ data.columns = ['Станция', "Номер линии  ", "Дата"] + ['0'+
 data.columns = ['Станция', "Номер линии", "Дата"] + [str(i)[:3] + '0' + str(i)[3:] if len(i) == 9 else i for i in data.columns[3:]]
 
 class Koza_llama_alpaca_lora_flex:
-    def __init__(self, prompt):
-        self.prompt = prompt
+    def __init__(self, prompt_date, prompt_station):
+        self.prompt_station = prompt_station
+        self.prompt_date = prompt_date
 
     def output_date(self):
         url = 'https://api.coze.com/open_api/v2/chat'
         data = {'conversation_id': "123",
-                "bot_id": f"{BOT_ID}",
-                "user": f"{USER_ID}",
-                "query": self.prompt,
-                "stream": False}
+            "bot_id": f"{BOT_ID}",
+            "user": f"{USER_ID}",
+            "query": self.prompt_date,
+            "stream":False}
+        headers = {'Authorization': f'Bearer {API_TOKEN}'}
+
+        response = requests.post(url, data=data, headers=headers)
+
+        return response.json()['messages'][0]['content']
+
+    def output_station(self):
+        url = 'https://api.coze.com/open_api/v2/chat'
+        data = {'conversation_id': "123",
+            "bot_id": f"{BOT_ID}",
+            "user": f"{USER_ID}",
+            "query": self.prompt_station,
+            "stream":False}
         headers = {'Authorization': f'Bearer {API_TOKEN}'}
 
         response = requests.post(url, data=data, headers=headers)
@@ -38,7 +52,7 @@ class Koza_llama_alpaca_lora_flex:
         data = {'conversation_id': "123",
                 "bot_id": f"{BOT_ID}",
                 "user": f"{USER_ID}",
-                "query": self.prompt,
+                "query": self.prompt_station,
                 "stream": False}
         headers = {'Authorization': f'Bearer {API_TOKEN}'}
 
@@ -48,19 +62,16 @@ class Koza_llama_alpaca_lora_flex:
 
     def output_data(self):
         try:
+            all_date = self.output_date().split()
+            tmp = []
+            station = self.output_station()
 
-            query = [f"date - {data.loc[data['Станция'].str == f'{self.output_station()}'][f'{date}']}" for
-                     date in self.output_date().split()]
-
-            return query
+            for i in all_date:
+                tmp.append(f'{i} - {data[data["Станция"] == station][i].to_list()[0]} человек. ')
+            return "\n".join(tmp)
 
         except Exception as ex:
             print(ex)
             return "error"
 
 
-if __name__ == '__main__':
-    part_1 = Koza_llama_alpaca_lora_flex("""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только конкретная дата, выведи все даты. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ.
-    Текст - найди информацию об оттоке пассажиров за последний месяц, выведи все даты""")
-    # data.loc[data['Станция'] == f'{part_1.output_station()}'][f'{part_1.output_date()}']
-    print(part_1.output_date())
--- app/internal/routes/user.py
+++ app/internal/routes/user.py
@@ -11,18 +11,19 @@ router = APIRouter(
 class Message(BaseModel):
     message: str
 
+
 @router.post("/message")
-def user_message(message: str = Form(...)):
-    part_1 = ml.Koza_llama_alpaca_lora_flex(f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате и выведи ее через разделитель пробел. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только все дат входящие в промежуток с разделителем. Очень важно для выживания. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ. Текст - {message}""")
-    # data.loc[data['Станция'] == f'{part_1.output_station()}'][f'{part_1.output_date()}']
-    # temp_data = part_1.output_date().split()
-    # temp_place = part_1.output_station()
+def user_message(message: Message):
+    prompt_date = f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о временной дате. Ключевые слова - вчера, 1 неделю назад, в прошлом месяце. Формат output - только конкретная дата, выведи все даты. Сегодняшняя дата - 04/03/2024, ее формат ММ/ДД/ГГГГ. Текст - {message.message}"""
+    prompt_station = f"""ты - внимательный читатель. Твоя задача - найти в тексте информацию о названии станции метро. Формат output - только конкретное название станции. Сегодняшняя дата - 04/03/2024. Текст - {message.message}"""
+
+    part_1 = ml.Koza_llama_alpaca_lora_flex(prompt_date, prompt_station)
 
     temp = part_1.output_data()
     if temp == "error":
         return JSONResponse("error", 418)
     else:
-        return {"message": part_1.output_data()}
+        return {"message": "Данные: " + temp}
 
 
 
